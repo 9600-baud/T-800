5feb5c8dbfe98dd1e6cc8973c5192ea4fd647397
T-800 <t800@9600-baud.net>
2016-03-31 23:10:40 -0400

    add 5 min timer to HackerHelp.pm

--- lib/T800/Plugin/HackerHelp.pm
+++ lib/T800/Plugin/HackerHelp.pm
@@ -1,11 +1,14 @@
 package T800::Plugin::HackerHelp;
 
 use Moose;
+use POE qw(Kernel Session);
 
 with 'T800::Role::Plugin';
 with 'T800::Role::MessageReceiver';
 with 'T800::Role::Initialization';
 with 'T800::Role::IRCHandler';
+
+has 'timeout' => ( is => 'rw', isa => 'Int', default => 1 );
 
 sub BUILD {
 	my $self = shift;
@@ -20,7 +23,17 @@ sub on_privmsg {
     $channel = $nick unless $channel =~ m/^#/;
     return unless $channel eq "#mainehackerclub";
     if ($what =~ m/^help$/iu) {
+        return unless $self->timeout;
         $self->irc->yield('privmsg', $channel, "I do apologize, but we need more information than just the word 'help'. We are a group of technology enthusiasts. We do not condone any illegal activities, such as 'cracking' and 'exploiting' systems. If you wish to learn about what we do, you can visit http://www.hackmaine.org/");
+        $self->timeout = 0;
+        POE::Session->create(
+            _start => sub {
+                $_[KERNEL]->alarm( resetto => time() + 300, 0 );
+            }
+            resetto => sub {
+                $self->timeout = 1;
+            }
+        );
     }
 }
 __PACKAGE__->meta->make_immutable;
